d99fe2bbb55767d9de5afc3e7902f513fd712f1d
Giorgio Gandola <giogandola@gmail.com>
2019-10-01 12:46:28 +0200

    initial commit2

--- src/classes/Video.java
+++ src/classes/Video.java
@@ -1,5 +1,8 @@
 package classes;
 
+/*
+classe statica, non ha bisogno di istanza ma puo accettare soltanto metodi statici
+ */
 public abstract class Video
 {
     public static void print(String s)
